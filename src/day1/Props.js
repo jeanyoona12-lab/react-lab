d927c9ec63a32111b3abbf0a7fbbe6cd00285769
jeanyoon.a12 <jeanyoon.a12@gmail.com>
2025-11-20 16:24:51 +0900

    update

--- src/day1/Props.js
+++ src/day1/Props.js
@@ -1,7 +1,8 @@
 const Props = (props)=>{
+    console.log (props.user);
     return (
         <div className="card">
-            <img src="/image/img-01.png"/>
+            <img src="./image/img-01.png"/>
             <h2>홍길동</h2>
             <p>우리의 영웅</p>
         </div>
