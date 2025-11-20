8420a3be058c1d8f06bea1eeb863f5ac328ba27a
jeanyoon.a12 <jeanyoon.a12@gmail.com>
2025-11-20 16:21:39 +0900

    update

---
+++ src/day1/Props.js
@@ -0,0 +1,10 @@
+const Props = (props)=>{
+    return (
+        <div className="card">
+            <img src="/image/img-01.png"/>
+            <h2>홍길동</h2>
+            <p>우리의 영웅</p>
+        </div>
+    );
+}
+export default Props;
