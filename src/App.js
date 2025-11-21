a4d5475187b458bcd09bf3c36db86ea6df01c978
jeanyoon.a12 <jeanyoon.a12@gmail.com>
2025-11-21 11:20:33 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Props from "./day1/Props";
+import Card from "./day2/Card";
+import './day2/App.css';
+
 const App = ()=>{
   return (
-    <div className="card">
-      <Props idx={1} text='AI 생태계와 새로워진 카카오톡이 선사하는 일상 혁신' text_2='일상의 AI 혁신' text_3='DAY1' />
-      <Props idx={2} text='카나나, AI Agent, MCP까지 카카오가 만드는 모두를 위한 기술' text_2='기술 하나의 가능성' text_3='DAY2' />
-      <Props idx={3} text='카카오 크루들이 나누는 인사이트 이야기' text_2='가능성을 만드는 사람들' text_3='KREW DAY' />
+    //화면에 보여지는 영역 : JSX 
+    <div className="card-wrap">
+      <Card file='1' title='DAY 1'/>
+      <Card file='2' title='DAY 2'/>
+      <Card file='3' title='Krew Day' />
     </div>
-    
-  );
+  ) ;
 }
 export default App;
---
+++ src/day2/Card.js
@@ -0,0 +1,13 @@
+import './card.css';
+//props : 부모-> 자식 : 객체로 전달 
+const Card = ({file,title})=>{
+    return(
+        <div className="card">
+            <div className="img-wrap">
+                <img src={`/image/day${file}.png`} alt="day1이미지" />
+                <h2>{title}</h2>
+            </div>
+        </div>
+    )
+};
+export default Card;
